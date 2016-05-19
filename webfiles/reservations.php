ee6c9246fe0b843b2b88756dbf34b97a204b7911
Sev Leonard <jennifer.m.leonard@gmail.com>
2016-05-19 13:26:59 -0700

    adding reservation mocks

---
+++ webfiles/reservations.php
@@ -0,0 +1,11 @@
+<?php 
+if(isset($_POST['submit'])){
+    startdate = $_POST['startdate'] 
+    departuredate = $_POST['departuredate']
+
+    echo "<div class='matchSummary'>" . rand(0,10) . " sites available</div>"
+   
+
+    
+    }
+?>
